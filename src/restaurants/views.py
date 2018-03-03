46561ef8236c40fc9c0a2a1b20f66dcf3b1ea0c0
Deep L Sukhwani <deepsukhwani@gmail.com>
2018-03-03 21:22:40 +0530

    Rendering HTML

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -6,5 +6,20 @@ from django.shortcuts import render
 
 # function based view
 def home(request):
-    return HttpResponse("Hello")
+    html_var = "f strings"
+    html_ = f"""<!DOCTYPE html>
+    <html lang=en>
+    
+    <head>
+    </head>
+    
+    <body>
+    <h1>Hello World!</h1>
+    <p1>This is {html_var} coming through</p1>
+    </body>
+    </html>
+    """
+
+    # f strings
+    return HttpResponse(html_)
     # return render(request, "home.html", {})  # response
