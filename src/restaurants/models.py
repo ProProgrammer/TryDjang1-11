f66d28969e0073e012d7223c049b60a5c072b3b0
Deep L Sukhwani <deepsukhwani@gmail.com>
2018-03-04 19:35:03 +0530

    Signal for Unique Slugs

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.db.models.signals import pre_save, post_save
+from .utils import unique_slug_generator
 
 # Create your models here.
 class RestaurantLocation(models.Model):
@@ -18,3 +19,25 @@ class RestaurantLocation(models.Model):
     @property
     def title(self):
         return self.name
+
+
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+
+# def rl_post_save_receiver(sender, instance, *args, **kwargs):
+#     print('saved...')
+#     print(instance.timestamp)
+#     print(instance.updated)
+    # Never call .save() in post_save signal receiver method as it will be an infinite loop.
+    # Evertime .save() is called, post_save signal will be called.
+    # You can do this though if you aren't setting instance slug in pre_save:
+    # if not instance.slug:
+    #     instance.slug = unique_slug_generator(instance)
+    #     instance.save()
+
+
+pre_save.connect(rl_pre_save_receiver, sender=RestaurantLocation)
+
+# post_save.connect(rl_post_save_receiver, sender=RestaurantLocation)
