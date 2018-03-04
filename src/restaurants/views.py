e3f43c132749bb65a546f13234c440974b5a64c7
Deep L Sukhwani <deepsukhwani@gmail.com>
2018-03-04 20:02:53 +0530

    Comment unused view and Get Single Items from the DB

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,19 +1,18 @@
 # Create your views here.
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from django.views.generic.list import ListView
 
 from .models import RestaurantLocation
 
 
-def restaurant_listview(request):
-    template_name = 'restaurants/restaurantlocation_list.html'
-    queryset = RestaurantLocation.objects.all()
-    context = {
-        'object_list': queryset
-    }
-    return render(request, template_name, context)
+# def restaurant_listview(request):
+#     template_name = 'restaurants/restaurantlocation_list.html'
+#     queryset = RestaurantLocation.objects.all()
+#     context = {
+#         'object_list': queryset
+#     }
+#     return render(request, template_name, context)
 
 
 class RestaurantListView(ListView):
