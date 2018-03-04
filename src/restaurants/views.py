103bd3e5af24329c29ac08fe68ef2a917f864bb3
Deep L Sukhwani <deepsukhwani@gmail.com>
2018-03-05 04:47:31 +0530

    The Extra Power of Django Model Forms

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -2,22 +2,26 @@
 from django.db.models import Q
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView
 from django.views.generic.list import ListView
 
-from .forms import RestaurantCreateForm
+from .forms import RestaurantLocationCreateForm
 from .models import RestaurantLocation
 
 
 def restaurant_createview(request):
-    form = RestaurantCreateForm(request.POST or None)
+    form = RestaurantLocationCreateForm(request.POST or None)
     errors = None
     if form.is_valid():
-        obj = RestaurantLocation.objects.create(
-            name=form.cleaned_data.get("name"),
-            location=form.cleaned_data.get("location"),
-            category=form.cleaned_data.get("category")
-        )
+        # customize
+        # pre_save signals
+        form.save()
+        # post_save signals
+        # obj = RestaurantLocation.objects.create(
+        #     name=form.cleaned_data.get("name"),
+        #     location=form.cleaned_data.get("location"),
+        #     category=form.cleaned_data.get("category")
+        # )
         return HttpResponseRedirect("/restaurants")
     if form.errors:
         errors = form.errors
@@ -52,3 +56,9 @@ class RestaurantListView(ListView):
 
 class RestaurantDetailView(DetailView):
     queryset = RestaurantLocation.objects.all()
+
+
+class RestaurantCreateView(CreateView):
+    form_class = RestaurantLocationCreateForm
+    template_name = 'restaurants/form.html'
+    success_url = '/restaurants'
