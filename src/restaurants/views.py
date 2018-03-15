a44a4c66595514b609c625ab243b8bbf2f1fe0ec
Deep L Sukhwani <deepsukhwani@gmail.com>
2018-03-15 21:28:46 +0530

    37 - Personalize Items

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,29 +1,20 @@
 # Create your views here.
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Q
-from django.views.generic import DetailView, CreateView
+from django.views.generic import DetailView, CreateView, UpdateView
 from django.views.generic.list import ListView
 
 from .forms import RestaurantLocationCreateForm
 from .models import RestaurantLocation
 
 
-class RestaurantListView(ListView):
+class RestaurantListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
-        slug = self.kwargs.get('slug')
-
-        queryset = RestaurantLocation.objects.all()
-
-        if slug:
-            return queryset.filter(
-                Q(category__iexact=slug) |
-                Q(category__icontains=slug)
-            )
-        return queryset
+        return RestaurantLocation.objects.filter(owner=self.request.user)
 
 
-class RestaurantDetailView(DetailView):
-    queryset = RestaurantLocation.objects.all()
+class RestaurantDetailView(LoginRequiredMixin, DetailView):
+    def get_queryset(self):
+        return RestaurantLocation.objects.filter(owner=self.request.user)
 
 
 class RestaurantCreateView(LoginRequiredMixin, CreateView):
@@ -47,3 +38,23 @@ class RestaurantCreateView(LoginRequiredMixin, CreateView):
         context = super(RestaurantCreateView, self).get_context_data(**kwargs)
         context['title'] = 'Add Restaurant'
         return context
+
+
+class RestaurantUpdateView(LoginRequiredMixin, UpdateView):
+    form_class = RestaurantLocationCreateForm
+    # The login_url variable here will override the one that we have set in default settings (base.py)
+    login_url = '/login/'  # We are keeping it same for now. But in case someone changes there, it will remain Login
+    #  for this view always until we change it here too or remove this variable altogether.
+    template_name = 'restaurants/detail-update.html'
+
+    # success_url = '/restaurants'
+
+    # Overriding form_valid method while implementing class based authentication
+
+    def get_context_data(self, **kwargs):
+        context = super(RestaurantUpdateView, self).get_context_data(**kwargs)
+        context['title'] = f'Update Restaurant: {self.get_object().name}'
+        return context
+
+    def get_queryset(self):
+        return RestaurantLocation.objects.filter(owner=self.request.user)
