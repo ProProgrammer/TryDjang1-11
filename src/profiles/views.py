82824c9a193f93c1614f49b8206884db58775d97
Deep L Sukhwani <deepsukhwani@gmail.com>
2018-03-17 15:17:55 +0530

    Basic implementation of search functionality on profile page

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView
+from restaurants.models import RestaurantLocation
+from menus.models import Item
 
 User = get_user_model()
 
@@ -14,3 +16,16 @@ class ProfileDetailView(DetailView):
         if username is None:
             raise Http404
         return get_object_or_404(User, username__iexact=username, is_active=True)
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileDetailView, self).get_context_data(**kwargs)
+        # print(context)
+        user = context['user']
+        query = self.request.GET.get('q')
+        items_exists = Item.objects.filter(user=user).exists()
+        qs = RestaurantLocation.objects.filter(owner=user)
+        if query:
+            qs = qs.filter(name__icontains=query)
+        if items_exists and qs.exists():
+            context['locations'] = qs
+        return context
