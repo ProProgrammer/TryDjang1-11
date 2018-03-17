633aca363ba87db339941227078c8255a4c1e899
Deep L Sukhwani <deepsukhwani@gmail.com>
2018-03-17 21:12:36 +0530

    41 - Follow Users

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -9,8 +9,8 @@ User = settings.AUTH_USER_MODEL
 
 class Profile(models.Model):
     user = models.OneToOneField(User)  # ForeignKey --> user.profile_set, OneToOne --> user.profile
-    followers = models.ManyToManyField(User, related_name='followers', blank=True)  # user.followers.all()
-    following = models.ManyToManyField(User, related_name='following', blank=True)  # user.following.all()
+    followers = models.ManyToManyField(User, related_name='is_following', blank=True)  # user.followers.all()
+    # following = models.ManyToManyField(User, related_name='following', blank=True)  # user.following.all()
     activated = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -22,6 +22,11 @@ class Profile(models.Model):
 def post_save_user_receiver(sender, instance, created, *args, **kwargs):
     if created:
         profile, is_created = Profile.objects.get_or_create(user=instance)
+        default_user_profile = Profile.objects.get_or_create(user__id=1)[0]
+        default_user_profile.followers.add(instance)
+        # default_user_profile.save() This is not required for ManytoMany fields
+        profile.followers.add(default_user_profile.user)
+        profile.followers.add(2)
 
 
 post_save.connect(post_save_user_receiver, sender=User)
